e8228ced4e3449e2e7be7dd48346557cd381fa3b
AhmedAbdelnaby <ahmedabnaby.97@gmail.com>
2022-09-08 12:59:46 +0200

    .

--- assets/js/email.js
+++ assets/js/email.js
@@ -3,6 +3,7 @@ function sendMail()
     var templateParams = {
         from_name: document.getElementById("name").value,
         email: document.getElementById("email").value,
+        subject: document.getElementById("subject").value,
         message: document.getElementById("message").value
     }
     emailjs.send('service_flpofzf', 'template_mctvtds', templateParams).then(function(response) {
